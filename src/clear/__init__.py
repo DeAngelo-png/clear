3d50a66f745086e26aee45e865495aaffe0e6692
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2024-04-11 07:46:07 -0700

    Update __init__.py
    Using an ASCII code that works on all operating systems terminal, an even shorter 1 liner that just works.

--- src/clear/__init__.py
+++ src/clear/__init__.py
@@ -4,14 +4,12 @@ Special thanks to David George for allowing me to use the Clear name on PyPI.
 
 A cross-platform Python module that provides a clear() function which clears the terminal. That's all.
 
-This function runs the one-liner: os.system('cls' if os.name == 'nt' else 'clear')
+This function clears the terminal screen by printing an ascii code, works on all systems and an even simpler 1 liner
 
 This module exists as a simpler alternative to defining this function on your own."""
 
-__version__ = '2.0.0'
-
-import os
+__version__ = '2.0.1'
 
 def clear():
-	"""Clears the terminal screen by running cls (on Windows) or clear (on macOS/Linux)."""
-	os.system('cls' if os.name == 'nt' else 'clear')
+	"""Simple clear function"""
+	print("\33c")
